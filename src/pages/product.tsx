3eea1e44095ae061db013af3d9f1c58f38714d7e
Ashish Shrestha <aashishshrestha5532@gmail.com>
2024-05-08 15:23:12 -0400

    product page with cart handling

---
+++ src/pages/product.tsx
@@ -0,0 +1,69 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { useCart } from "../provider/cart-provider";
+import { IProduct } from "../interface/product-interface";
+
+const GET_PRODUCT = gql`
+  query GetProduct($id: ID!) {
+    product(id: $id) {
+      id
+      name
+      description
+      image
+      price
+      rating
+      releated {
+        id
+        name
+        image
+        rating
+      }
+    }
+  }
+`;
+
+const ProductDetail = () => {
+  const { id } = useParams();
+
+  const { loading, error, data } = useQuery(GET_PRODUCT, {
+    variables: { id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return `Error! ${error}`;
+
+  const { product }: { product: IProduct } = data;
+
+  const { cart, addItemToCart, reduceItemQuantity } = useCart();
+
+  const item = cart.find((item) => item.id === product.id);
+
+  return (
+    <div>
+      <h2>{product.name}</h2>
+
+      <img
+        src={product.image}
+        className="product-detail-image"
+        alt={product.name}
+      />
+      <div>
+        Price: ${product.price} <br />
+        Rating: {product.rating} <br />
+        <b>{product.isAvailable ? "In Stock" : "Out of Stock"}</b>
+        <p>{product.description}</p>
+      </div>
+
+      <button
+        onClick={() => {
+          addItemToCart(product);
+        }}
+      >
+        +
+      </button>
+      <span>{item?.quantity ?? 0}</span>
+      <button onClick={() => reduceItemQuantity(product.id)}>-</button>
+    </div>
+  );
+};
+export default ProductDetail;
